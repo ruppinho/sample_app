d54f641abe01c768e2039c3a4aab349f42cb3753
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-09 12:17:43 +0200

    Refactored application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
 
+  def base_title
+    @base_title ||= "Ruby on Rails Tutorial Sample App"
+  end
+
   # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
+  def full_title(page_title = nil)
+    if page_title.nil? || page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
