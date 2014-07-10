0a1f4c716286c5a9eaed83074c6473edaf4128a1
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-10 15:52:30 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'user#new',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
 end
