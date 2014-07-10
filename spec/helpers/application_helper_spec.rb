04d73706246068ed4897035a63ee2a071fc92844
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-10 15:11:34 +0200

    Improved specs

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,19 @@
+
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe '#full_title' do
+    it 'includes the page title' do
+      expect(full_title('foo')).to match(/foo/)
+    end
+
+    it 'includes the base title' do
+      expect(full_title('foo')).to match(/^Ruby on Rails Tutorial Sample App/)
+    end
+
+    it 'does not include a bar for the home page' do
+      expect(full_title('')).not_to match(/\|/)
+    end
+  end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,85 +1,62 @@
 require 'spec_helper'
 
-describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+describe 'Static pages' do
+
+  shared_examples_for 'a static page' do
+    its 'title contains the base title' do
+      expect(page).to have_title(full_title(page_title))
+    end
+
+    its 'heading contains the page title' do
+      expect(page).to have_selector('h1', text: heading)
+    end
+  end
 
   describe 'Home page' do
-    let(:page_title) { 'Home' }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
     before :each do
       visit root_path
     end
 
-    it "has the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it 'has the base title' do
-      expect(page).to have_title(base_title)
-    end
+    it_behaves_like 'a static page'
 
     it 'does not have a custom page title' do
-      expect(page).to_not have_title(" | #{page_title}")
+      expect(page).to_not have_title(' | Home')
     end
   end
 
   describe 'Help page' do
     let(:page_title) { 'Help' }
+    let(:heading)    { 'Help' }
 
     before :each do
       visit help_path
     end
 
-    it 'has the right content' do
-      expect(page).to have_content(page_title)
-    end
-
-    it 'has the base title' do
-      expect(page).to have_title(base_title)
-    end
-
-    it 'has a custom page title' do
-      expect(page).to have_title(" | #{page_title}")
-    end
+    it_behaves_like 'a static page'
   end
 
   describe 'About page' do
     let(:page_title) { 'About Us' }
+    let(:heading) { 'About Us' }
 
     before :each do
       visit about_path
     end
 
-    it 'has the right content' do
-      expect(page).to have_content(page_title)
-    end
-
-    it 'has the base title' do
-      expect(page).to have_title(base_title)
-    end
-
-    it 'has a custom page title' do
-      expect(page).to have_title(" | #{page_title}")
-    end
+    it_behaves_like 'a static page'
   end
 
   describe 'Contact page' do
     let(:page_title) { 'Contact' }
+    let(:heading) { 'Contact' }
 
     before :each do
       visit contact_path
     end
 
-    it 'has the right content' do
-      expect(page).to have_content(page_title)
-    end
-
-    it 'has the base title' do
-      expect(page).to have_title(base_title)
-    end
-
-    it 'has a custom page title' do
-      expect(page).to have_title(" | #{page_title}")
-    end
+    it_behaves_like 'a static page'
   end
 end
