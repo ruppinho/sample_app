1ff4943636af1a784d37c8910df093fa0e7b7480
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 16:43:06 +0200

    Finished authentication behavior

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,24 @@ class User < ActiveRecord::Base
   # Callbacks
   #
   before_save { self.email.downcase! }
+  before_create :create_remember_token
+
+  #
+  # Methods
+  #
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
 
 end
