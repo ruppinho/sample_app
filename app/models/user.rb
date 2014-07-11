92bbb78cbc56aef37c4f5c46f6766f0175f2426b
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 09:25:56 +0200

    Improved user model + specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+  before_save { self.email = email.downcase }
+
 end
