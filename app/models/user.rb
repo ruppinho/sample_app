65ad11debf7e01cf7b19c44b8907dc9939c0ac07
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 10:27:26 +0200

    Improved email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   validates_length_of :name, maximum: 50
 
   validates_presence_of :email
-  validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates_uniqueness_of :email, case_sensitive: false
 
   validates_length_of :password, minimum: 6
@@ -20,6 +20,6 @@ class User < ActiveRecord::Base
   #
   # Callbacks
   #
-  before_save { self.email = email.downcase }
+  before_save { self.email.downcase! }
 
 end
