1ece0ec62585dc2446998b47309d4e75b2e31dff
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 15:42:56 +0200

    Added authentication skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 SampleApp::Application.routes.draw do
+  root  'static_pages#home'
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
 
-  root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
