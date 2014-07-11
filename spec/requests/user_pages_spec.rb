8b2b65e09d55f8ee5b15eddf74459b207a3dc535
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 11:03:36 +0200

    Added user profile page skeleton

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,4 +24,12 @@ describe 'User pages' do
       expect(page).to have_title(" | #{page_title}")
     end
   end
+
+  describe 'profile page' do
+    # Replace with code to make a user variable
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
