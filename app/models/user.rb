1ef55df996deeee7cc2f4c36eafcd2703f6b1e6b
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-11 10:12:25 +0200

    Improved user model + specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_uniqueness_of :email, case_sensitive: false
 
+  validates_length_of :password, minimum: 6
+
   #
   # Callbacks
   #
