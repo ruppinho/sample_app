2c78ae1fde18d26b656ab3c31337f3fb7e13804b
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-13 00:23:01 +0200

    Created partial for creating/editing users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -118,7 +118,7 @@ describe 'User pages' do
         fill_in 'Name',         with: 'Example User'
         fill_in 'Email',        with: 'user@example.com'
         fill_in 'Password',     with: 'foobar'
-        fill_in 'Confirmation', with: 'foobar'
+        fill_in 'Confirm Password', with: 'foobar'
       end
 
       it 'should create a user' do
@@ -184,6 +184,6 @@ describe 'User pages' do
       end
       specify { expect(user.reload).not_to be_admin }
     end
-    
+
   end
 end
