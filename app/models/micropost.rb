1389d56d5a5f8aebbf3cd035cbd9788cdbf71677
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-17 12:43:07 +0200

    Codesytle

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,7 +13,7 @@ class Micropost < ActiveRecord::Base
 
   validates :content,
             presence: true,
-            length: { maximum: 140 }
+            length:   { maximum: 140 }
 
   #
   # Scopes
@@ -25,8 +25,7 @@ class Micropost < ActiveRecord::Base
   #
   class << self
     def from_users_followed_by(user)
-      followed_user_ids = "SELECT followed_id FROM relationships
-                         WHERE follower_id = :user_id"
+      followed_user_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
       where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
             user_id: user.id)
     end
