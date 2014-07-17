1a563f47fa5f436498688242a33d0a1f90d5c558
Paul Ruppersberger <paul@ruppersberger.de>
2014-07-17 12:35:51 +0200

    Refactored users feed to reflect all followed users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,4 +19,16 @@ class Micropost < ActiveRecord::Base
   # Scopes
   #
   default_scope { order('created_at DESC') }
+
+  #
+  # Methods
+  #
+  class << self
+    def from_users_followed_by(user)
+      followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+      where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+            user_id: user.id)
+    end
+  end
 end
